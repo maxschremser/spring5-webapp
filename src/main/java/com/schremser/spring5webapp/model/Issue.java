b2453c663124dd2f723c3e3237f2d271cdec52ab
bluemax <maxi@schremser.com>
2018-01-07 10:01:18 +0100

    Initial commit

---
+++ src/main/java/com/schremser/spring5webapp/model/Issue.java
@@ -0,0 +1,12 @@
+package com.schremser.spring5webapp.model;
+
+import javax.persistence.Entity;
+
+/**
+ * Created by bluemax on 07.01.18.
+ */
+@Entity
+public class Issue {
+    private Long id;
+    
+}
