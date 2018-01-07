e53b6f9cec9968d9f450d1fe3a7654862284e16d
bluemax <maxi@schremser.com>
2018-01-07 10:39:45 +0100

    #1 - Create Issue Entity

--- src/main/java/com/schremser/spring5webapp/model/Issue.java
+++ src/main/java/com/schremser/spring5webapp/model/Issue.java
@@ -1,6 +1,7 @@
 package com.schremser.spring5webapp.model;
 
 import javax.persistence.Entity;
+import java.util.List;
 
 /**
  * Created by bluemax on 07.01.18.
@@ -8,5 +9,11 @@ import javax.persistence.Entity;
 @Entity
 public class Issue {
     private Long id;
-    
+    private String title;
+    private String comment;
+    private String pipeline;
+    private List<String> assignees;
+    private List<String> labels;
+    private List<String> projects;
+
 }
